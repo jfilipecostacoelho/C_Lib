ead44459b8d54673dd6caf7014c8049ef2609543
João Filipe Costa Coelho <74990822+jfilipecostacoelho@users.noreply.github.com>
2022-11-30 15:48:27 +0000

    Update myclib.h

--- myclib.h
+++ myclib.h
@@ -1,5 +1,8 @@
 // Last update  30/11/2022
 
 
-// Removes duplicates from an array
+// Removes duplicates from a sorted array
 int RemoveDups(int v[], int n);
+
+// Sorts an array
+void Sort(int v[], int n)
