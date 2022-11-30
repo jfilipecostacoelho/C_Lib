d90f6e18f4d8920b2fc4243e7a214062d33f1d59
João Filipe Costa Coelho <74990822+jfilipecostacoelho@users.noreply.github.com>
2022-11-30 15:48:07 +0000

    Update myclib.c

--- myclib.c
+++ myclib.c
@@ -1,3 +1,6 @@
+/*
+Removes duplicates numbers from a sort array
+*/
 int RemoveDups(int arr[], int n)
 {
     int temp[n];
@@ -18,4 +21,28 @@ int RemoveDups(int arr[], int n)
     }
   
     return j;
+}
+
+/*
+Sort Array
+*/
+void Sort(int v[], int n)
+{
+    int aux = 0, swap = 0;;
+    
+    for(int i=0;i<n-1;i++)
+    {
+        if(v[i] > v[i+1])
+        {
+            aux = v[i];
+            v[i] = v[i+1];
+            v[i+1] = aux;
+            swap = 1;
+        }
+    }
+    
+    if(swap == 1)
+    {
+       Sort(v, 1000);
+    }
 }
